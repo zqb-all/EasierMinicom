0f8fc1d11dd24914a4cae716fd339ce8bb8761c5
zqb-all <zhuangqiubin@gmail.com>
2018-10-14 17:17:23 +0800

    first version

---
+++ EasierMinicom.sh
@@ -0,0 +1,15 @@
+com() {
+	ports_USB=$(ls /dev/ttyUSB*)
+	ports_ACM=$(ls /dev/ttyACM*)  #arduino
+	ports="$ports_USB $ports_ACM"
+	datename=$(date +%Y%m%d-%H%M%S)
+	select port in $ports;do
+		if [ "$port" ]; then
+		    echo "You select the choice '$port'"
+		    minicom -D "$port" -C /tmp/"$datename".log "$@"
+		    break
+		else
+		    echo "Invaild selection"
+	    fi
+	done
+}

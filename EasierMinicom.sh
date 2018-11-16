d569985baa4506b762077b4935bf494b158a532a
zqb-all <zhuangqiubin@gmail.com>
2018-11-16 08:51:44 +0800

    support detect lock and auto choose
    1.only list unlock devices
    2.auto choose when only one unlock device
    3.ask if save log to another dir when exit
    
    Signed-off-by: zqb-all <zhuangqiubin@gmail.com>

--- EasierMinicom.sh
+++ EasierMinicom.sh
@@ -1,15 +1,43 @@
+
 com() {
-	ports_USB=$(ls /dev/ttyUSB* 2>/dev/null)
-	ports_ACM=$(ls /dev/ttyACM* 2>/dev/null)  #arduino
+	local ports_USB ports_ACM ports datename dev devs dev_count
+	ports_USB=$(ls /dev/ttyUSB* 2>null |  xargs -I {} basename {})
+	ports_ACM=$(ls /dev/ttyACM* 2>null |  xargs -I {} basename {})
 	ports="$ports_USB $ports_ACM"
+
+	#check lock
+	devs=""
+	dev_count=0
+	for dev in ${ports};
+	do
+		! ls /run/lock/*"${dev}"* &> /dev/null && { devs+="${dev} ";((dev_count++)); }
+	done;
+	[ -z "$devs" ] && echo "No Unlock Devices" && return 0;
+
 	datename=$(date +%Y%m%d-%H%M%S)
-	select port in $ports;do
-		if [ "$port" ]; then
-		    echo "You select the choice '$port'"
-		    minicom -D "$port" -C /tmp/"$datename".log "$@"
-		    break
-		else
-		    echo "Invaild selection"
-	    fi
-	done
+	if [ $dev_count -eq 1 ]; then
+		dev=$devs
+	else
+		#select dev to open
+		echo "Please select one device: (Ctrl+C to abort)"
+		select dev in $devs;do
+			if [ "$dev" ]; then
+			    echo "You select the '$dev'"
+			    break
+			else
+			    echo "Invaild selection"
+		    fi
+		done
+	fi
+
+	out="/tmp/$dev-$datename.log"
+	keep_dir="${HOME}/minicom_keep"
+
+	minicom -D "/dev/$dev" -C "${out}" "$@"
+
+	[ -f "${out}" ] && {
+		echo log : "${out}"
+		read -p "Keep it? [Y|n]: " keep;
+		[ "${keep}" = 'Y' -o "${keep}" = 'y' ] && { mkdir -p "$keep_dir"; cp "${out}" "$keep_dir"; echo "saved in $keep_dir"; }
+	}
 }
--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## What is EasierMinicom
 EasierMinicom is a shell script to help you use minicom easier.
 
-* auto detect devices, then list them for user to choose
-* auto save log to /tmp
+* auto detect unlock devices, then list them for user to choose. if only one device then auto choose it.
+* auto save log to /tmp, when exit, ask if save to another dir.
 
 ## How to use
 ```bash
@@ -18,11 +18,12 @@ com
 
 EasierMinicom 帮助你更简单地使用minicom的shell脚本。
 
-* 自动检测并列出设备给用户选择
-* 自动保存log到/tmp目录
+* 自动检测未锁定的设备，如果有多个则列出给用户选择，如果只有一个则直接使用。
+* 自动保存log到/tmp目录，退出时，询问是否要保存到另外的目录。
 
 ## 如何使用
 ```bash
 echo "source $(pwd)/EasierMinicom.sh" >> ~/.bashrc
 source ~/.bashrc
 com
+```
